a5e9d7099b292b06dff175a50f5985545e4b719b
Sahil-pillania <sahilpillania88@gmail.com>
2022-05-01 08:08:30 +0530

    Committed

--- src/CreateNote.jsx
+++ src/CreateNote.jsx
@@ -5,7 +5,7 @@ const CreateNote = () => {
     return <>
         <div className="main_note">
             <form>
-                <input type="text" placholder="Title"/>
+                <input type="text" placeholder="Title" autoComplete="off" />
                 <textarea rows="" column="" placeholder="Write a note..."></textarea>
                 <Button>
                         <AddIcon className="plus_sign" />
---
+++ src/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Note =() => {
+        <div className="note">
+            <h1> title</h1>
+            <br/>
+            <p> This is the content.</p>
+        </div>
+}
+ export default Note;
